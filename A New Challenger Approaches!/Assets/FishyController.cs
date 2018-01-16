721181823ebbb68d064836d34560a8691f75072c
Luke <leeluke_9@live.com>
2018-01-16 21:43:05 +0800

    - something's wrong with my sourcetree...

---
+++ A New Challenger Approaches!/Assets/FishyController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishyController : UnitInput {
+
+	// Constants
+	protected const string READY_TRIGGER = "FishyReady";
+	protected const string LAZOR_TRIGGER = "FishyLazor";
+	protected const string BUBBLEBEAM_TRIGGER = "FishyBubblebeam";
+	protected const string PLAYER_LAYER = "Player";
+
+	[Header("Delay")]
+	// Delay
+	[SerializeField]
+	protected float minCoolown;
+	[SerializeField]
+	protected float maxCooldown;
+
+	// Runtime variables
+	protected bool hasEnteredCombat = false;
+	protected bool isDoingAction = false;
+	protected float cooldownToNextAction = 0;
+	protected int totalWeights;
+	protected float initialScale;
+	protected bool isDead = false;
+
+	// References
+	[SerializeField]
+	protected List<Transform> targetCharacters;
+	[SerializeField]
+	protected Transform fishyLure;
+	[SerializeField]
+	protected Transform fishyLureAim;
+
+	[Header("Lazerbeam")]
+	[SerializeField]
+	protected int numberOfLaserbeams;
+	[SerializeField]
+	protected float laserbeamInterval;
+	[SerializeField]
+	protected float lazerbeamDamage;
+	[SerializeField]
+	protected float lazerbeamSpeed;
+	[SerializeField]
+	protected float indicatorRadius;
+	protected Transform fishyLureTarget;
+
+	// Components
+	protected Animator characterAnimator;
+
+	protected override void Awake() {
+		base.Awake();
+		characterAnimator = GetComponent<Animator>();
+		//totalWeights = moveWeight + stabWeight + spitfireWeight;
+		initialScale = transform.localScale.x;
+		fishyLureTarget = targetCharacters [Random.Range (0, targetCharacters.Count)];
+	}
+
+	protected void Update() {
+		Vector3 dirToTarget = (fishyLureTarget.position - fishyLure.position).normalized;
+		fishyLureAim.localPosition = fishyLure.position + dirToTarget * indicatorRadius;
+	}
+
+	protected IEnumerator ApplyCooldownDelay() {
+		isDoingAction = true;
+		yield return new WaitForEndOfFrame();
+		yield return new WaitForSeconds(characterAnimator.GetCurrentAnimatorStateInfo(0).length);
+		isDoingAction = false;
+		cooldownToNextAction = Random.Range (minCoolown, maxCooldown);
+	}
+
+	public void PrepLazerbeam() {
+
+	}
+
+	public void Lazerbeam() {
+		StartCoroutine (FireLazerbeams ());
+	}
+
+	protected IEnumerator FireLazerbeams() {
+		for (int i = 0; i < numberOfLaserbeams; i++) {
+
+		}
+	}
+
+}
---
+++ A New Challenger Approaches!/Assets/LazorbeamProjectile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazorbeamProjectile : Projectile {
+
+	// Runtime variables
+	protected float projectileGravity;
+
+	public void SetupProjectile(float damage, Vector2 velocity, float lifespan, float gravity, GameObject hitEffect) {
+		projectileDamage = damage;
+		projectileSpeed = velocity.magnitude;
+		projectileLifespan = lifespan;
+		unitProjectileDirection = velocity.normalized;
+		projectileGravity = gravity;
+		projectileRigidbody.velocity = velocity;
+		projectileHitEffect = hitEffect;
+	}
+
+	protected override void UpdateProjectile() {
+		projectileRigidbody.velocity += new Vector2(0, projectileGravity * Time.deltaTime);
+	}
+
+	protected override void OnHitFriendly (GameObject hitObject) { }
+
+	protected override void OnHitEnemy (GameObject hitObject) {
+		CameraController.Instance.ShakeCamera (0.075f, .75f);
+		Instantiate (projectileHitEffect, transform.position, Quaternion.Euler (Vector2.zero));
+		hitObject.GetComponent<UnitAttributes> ().ApplyAttack (projectileDamage, transform.position, projectileBuffs);
+	}
+
+	protected override void OnHitStructure(GameObject hitObject) {
+		RaycastHit2D groundHit = Physics2D.Raycast(transform.position, Vector2.down, transform.lossyScale.y, LayerMask.GetMask(STRUCTURE_LAYER));
+		Instantiate (projectileHitEffect, groundHit.point, Quaternion.Euler(Vector3.zero));
+		float angleOfGround = Vector2.SignedAngle(groundHit.normal, Vector2.up);
+		OnProjectileDeath ();
+	}
+
+	protected override void OnProjectileDeath () {
+		Destroy (this.gameObject);
+	}
+}
